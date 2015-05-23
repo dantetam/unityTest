be4962d6f71b724237724fc470e28d07239de1c9
dantetam <dante.tam1@gmail.com>
2015-05-23 15:28:12 -0700

    First commit

---
+++ Assets/Scripts/MoverScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoverScript : MonoBehaviour
+{
+	public float speed;
+	
+	void Start ()
+	{
+		GetComponent<Rigidbody>().velocity = transform.forward * speed;
+	}
+	
+}
---
+++ Assets/Scripts/RotateScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateScript : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag("Shot"))
+		{
+		    this.gameObject.SetActive(false);
+		}
+	}
+	
+}
